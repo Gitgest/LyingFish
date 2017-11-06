098ee008d8f0c9743a1372fcfe49ccd9c261e5d6
Unibuni7 <abdu0445@edu.easj.dk>
2017-11-06 15:56:28 +0100

    CheckDeck unit test

--- UnitTest.js
+++ UnitTest.js
@@ -18,15 +18,69 @@ function canLie (a) {
 var unitTest = {};
 
 unitTest.testLie = function (method) {
-    var num1 = 3
+    var num1 = 3;
     var result = num1 - 1;
 
     if (method(num1) === result){
-        true
+        return true;
     } else {
-        false
+         return false;
     }
 
 };
 
-console.log(unitTest.testLie(canLie));
+// console.log(unitTest.testLie(canLie));
+
+/*function checkDeck2(x,z){
+    var deck1 = [a,b,c];
+    var deck2 = [d,e,a];
+
+
+    if (click == a){
+        x = a;
+        z = x.contain.call(deck2,a);
+        return true;
+    } else {
+       return false;
+    }
+}*/
+
+function checkDeck(Array,card) {
+
+    var isAmatch = false;
+    for(i=0,i<Array.length; i++;){
+        if(card===Array[i]){
+            isAmatch = true;
+        }
+    }
+}
+
+unitTest.testCheckTrue = function (method) {
+    var Array = [1, 2, 3, 4, 5];
+    var num2 = 5;
+    var result = true;
+
+    if (method(num1, num2) === result) {
+       return true;
+    } else {
+        return false;
+    }
+};
+
+    unitTest.testCheckFalse = function (method) {
+        var Array = [1,2,3,4,5];
+        var num2 = 6;
+        var result = false;
+
+        if (method(num1,num2) === result){
+            return true;
+        } else {
+            return false;
+        }
+
+
+    };
+
+
+console.log(unitTest.testCheckTrue(checkDeck));
+console.log(unitTest.testCheckFalse(checkDeck));
